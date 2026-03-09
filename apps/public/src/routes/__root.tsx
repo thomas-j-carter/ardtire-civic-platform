21b5b38154dbe112d4b884d5c5898b0727e13ea2
thomas-j-carter <thomas.carter@appliedinnovationcorp.com>
2026-03-09 03:34:34 -0400

    fix(tailwind): wire tailwind v4 styles and vite plugin

--- apps/public/src/routes/__root.tsx
+++ apps/public/src/routes/__root.tsx
@@ -1,41 +1,13 @@
-import {
-  HeadContent,
-  Outlet,
-  Scripts,
-  createRootRouteWithContext,
-} from '@tanstack/solid-router'
-import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools'
-
-import { HydrationScript } from 'solid-js/web'
-import { Suspense } from 'solid-js'
-
-import Header from '../components/Header'
-
-import styleCss from '../styles.css?url'
+import { createRootRoute, Outlet } from '@tanstack/solid-router'
 import appCss from '../styles.css?url'
 
-export const Route = createRootRouteWithContext()({
+export const Route = createRootRoute({
   head: () => ({
-    links: [{ rel: 'stylesheet', href: appCss }, { rel: 'stylesheet', href: styleCss }],
+    links: [{ rel: 'stylesheet', href: appCss }],
   }),
-  shellComponent: RootComponent,
+  component: Root,
 })
 
-function RootComponent() {
-  return (
-    <html>
-      <head>
-        <HydrationScript />
-      </head>
-      <body>
-        <HeadContent />
-        <Suspense>
-          <Header />
-          <Outlet />
-          <TanStackRouterDevtools />
-        </Suspense>
-        <Scripts />
-      </body>
-    </html>
-  )
+function Root() {
+  return <Outlet />
 }
